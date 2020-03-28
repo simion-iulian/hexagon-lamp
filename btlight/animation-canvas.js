b7828997ecede075723fd2efdbd63a68e65f7f2b
Simion Belea <simion.belea@rvu.co.uk>
2020-03-28 19:03:39 +0000

    animation canvas smoother

--- btlight/animation-canvas.js
+++ btlight/animation-canvas.js
@@ -5,7 +5,7 @@ const p5               = require('./noise.js')
 // "a hexagon with a long diagonal of 1.0000000 will have a distance of 0.8660254 between parallel sides."
 // this is handy to scale content on Y so it appears 1:1 on the hexagon, notice the hexagon in the LUT if you squint your eyes is not a 1:1 rectangle
 // const HEX_HEIGHT_RATIO = 0.8660254;
-const HEX_HEIGHT_RATIO = 0.8118988125;
+const HEX_HEIGHT_RATIO = 0.8128988125;
 // full canvas dimensions
 const LUT_W = 15;
 const LUT_H = 16;
@@ -40,8 +40,6 @@ let pastel = 10;
 // animation tests
 let frame = 0;
 let txt = 'Hello !';
-let scrollX = 0;
-let scrollW;
 let gradient;
 // perlin animation vars
 // animation is a port of https://processing.org/examples/noise3d.html
@@ -123,38 +121,40 @@ function updatePerlin(speed = 1) {
   zoff += zincrement; // Increment zoff
   // set pixels
   ctx.putImageData(ctxImageData,0,0);
-  
 }
 
+const speed_to_velocity = (speed) => {return 1 + (speed/100) - 0.05};
+
 function updateRipple(frame, ripple_speed = 1, pastel = 30) {
-  // every two frames add a ripple at a random location
-  const dampening = 0.92;
+  const dampening = 0.99;
+  const s_to_v = (speed_to_velocity(ripple_speed/3));
+  
   if(frame % 60 == 0){
-    // ripple_speed means add more ripples
-    for(let i = 0; i < ripple_speed; i++){
-      let rx = Math.floor(2 + Math.random() * 12);
-      let ry = Math.floor(2 + Math.random() * 12);
-      previous[rx][ry] = 500;
-    }
+    console.log(`updated frame: ${frame} - ${s_to_v}`)
+    previous[7][8] =500;
   }
   
   let ctxImageData = ctx.getImageData(0,0,LUT_W,LUT_H);
   let ctxData = ctxImageData.data;
   
   for (let i = 1; i < rows-1; i++) {
-      for (let j = 1; j < cols-1; j++) {
-        current[i][j] = (previous[i-1][j] + 
-                         previous[i+1][j] + 
-                         previous[i][j-1] + 
-                         previous[i][j+1]) * 0.5 - 
-                         current[i][j];
+    for (let j = 1; j < cols-1; j++) {
+
+        const smoothed = (previous[i-1][j] + 
+                          previous[i+1][j] + 
+                          previous[i][j-1] + 
+                          previous[i][j+1])/4;
+
+        const velocity = (- current[i][j])*s_to_v;
+
+        current[i][j] = smoothed*2 + velocity;
 
         current[i][j] = current[i][j] * dampening;
 
         let index = (i + j * LUT_W) * 4;// RGBA
         let gray  = Math.floor(current[i][j]);
 
-        ctxData[index+0] = (255 - gray);
+        ctxData[index+0] = gray;
         ctxData[index+1] = gray;
         ctxData[index+2] = gray;
         ctxData[index+3] = pastel;
@@ -180,8 +180,8 @@ function updateCircle(speed = 1 , pastel = 0){
   ctx.strokeStyle = '#000099';
   ctx.beginPath();
   const r = circle_speed * 4;
-  // ctx.ellipse(LUT_W * 0.5, LUT_H * 0.5, r, r * HEX_HEIGHT_RATIO, 0, 0, 2 * Math.PI);
-  ctx.ellipse(LUT_W * 0.5, LUT_H * 0.5, r, r, 0, 0, 2 * Math.PI);
+  ctx.ellipse(LUT_W * 0.5, LUT_H * 0.5, r * HEX_HEIGHT_RATIO, r, 0, 0, 2 * Math.PI);
+  //ctx.ellipse(LUT_W * 0.5, LUT_H * 0.5, r, r, 0, 0, 2 * Math.PI);
   ctx.stroke();
 }
 // main render loop for canvas animations
